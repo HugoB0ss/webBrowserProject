71b76e2d8dabbb40c68d19509b242c1266e5a426
Hugo <hugo.le-dall@etu.univ-nantes.fr>
2018-04-07 15:43:27 +0200

    Add metadata - need to add in redis

--- src/index.js
+++ src/index.js
@@ -1,63 +1,57 @@
 module.exports = (config) => {
-    let ineed = require('ineed');
+	let request = require('request');
+	let cheerio = require('cheerio');
     let toVisitArray = [];
     let visitedArray = [];
-    let errorCounter = 0;
-    let successCounter = 0;
-    // Initial page parsing
-    parsePage(config.baseUrl);
+    let errors = 0;
+    toVisitArray.push(config.baseUrl);   // Initial page parsing
     processArray();
 
 
 
     function processArray() {
+
         setInterval(function () {
-            var nextUrl = toVisitArray[0];
-            
-            // TO DELETE ---- ONLY FOR DEBUG
-            console.log(visitedArray);
-            console.log(errorCounter);
-            console.log(successCounter);
+            let nextUrl = toVisitArray[0];
+
             if (nextUrl) {
 				toVisitArray.shift();
-				parsePage(nextUrl);                
+				parsePage2(nextUrl);
             }
         }, 500);
     }
 
+    function parsePage2(url){
+        request(url,function(error,response,body){
+            if(error){
+                errors++;
+            }else{
+                let $ = cheerio.load(body);
+                addVisitedPage(url,$('title').text(),$('meta'));
+                $("a[href^='http']").each(function(){
+                    addWithExistenceCheck($(this).attr('href'));
 
-    function parsePage(url){
-        ineed.collect.title.hyperlinks.from(url,
-            function(err,response,result){
-
-                if(errorCounter >= 10 && errorCounter >= successCounter){
-                    throw new Error('Too many errors');
-                }
-                if(err){
-                    errorCounter++;
-                }else{
-					addVisitedPage(url,result.title);
-                    addWithExistenceCheck(result.hyperlinks);
-                    successCounter++;
-                }
-            });
-    }
-
-    function addWithExistenceCheck(urls){
-        urls.forEach((potentialUrl) => {
-            if (!hasBeenVisited(potentialUrl)) {
-                toVisitArray.push(potentialUrl.href);
+                });
             }
         });
     }
+
+    function addWithExistenceCheck(potentialUrl){
+        if (!hasBeenVisited(potentialUrl)) {
+            toVisitArray.push(potentialUrl);
+        }
+    }
 	
-	function addVisitedPage(url,title){
-		var visited = {
+	function addVisitedPage(url,title,metaList){
+        let visited = {
 			'url':url
-		}
+		};
 		if(title){
 			visited.title = title;
 		}
+		if(metaList){
+		    visited.metaList = metaList;
+        }
 		visitedArray.push(visited);
 	}
 
